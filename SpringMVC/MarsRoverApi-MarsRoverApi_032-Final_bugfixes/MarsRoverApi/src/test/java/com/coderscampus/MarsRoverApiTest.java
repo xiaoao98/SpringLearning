558720ccaa0a7d88479e97300f4ca2b35881b60a
Ao <yuza@harbdata.com>
2022-07-18 10:24:52 -0400

    springMVC

---
+++ SpringMVC/MarsRoverApi-MarsRoverApi_032-Final_bugfixes/MarsRoverApi/src/test/java/com/coderscampus/MarsRoverApiTest.java
@@ -0,0 +1,15 @@
+package com.coderscampus;
+
+import org.junit.Test;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import com.coderscampus.response.MarsRoverApiResponse;
+
+public class MarsRoverApiTest {
+
+  @Test
+  public void smallTest () {
+    
+  }
+}
